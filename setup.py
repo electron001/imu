95c3628ff21ec3591901e15549a90e1c04cbaf20
electron001 <65872400+electron001@users.noreply.github.com>
2021-03-05 14:29:52 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: Apache License 2.0',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 3.8',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(
+    name         = 'IMU_LSM9DS1',
+    version      = '1.0.1',
+    author       = 'Maciej Biskup',
+    author_email = 'mbiskup123@gmail.com',
+    description  = "Library for lsm9ds1",
+    long_description=long_description,
+    url          = 'https://github.com/electron001/imu/',
+    license      = 'Apache License 2.0',
+    classifiers  = classifiers,
+    packages     = find_packages()
+)
+
+
+  
