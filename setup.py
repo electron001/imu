c0b27c373aaa3be19c656788115177defe330671
electron001 <65872400+electron001@users.noreply.github.com>
2021-03-05 14:30:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ except:
 
 classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
-               'License :: OSI Approved :: Apache License 2.0',
+               'License :: OSI Approved :: Apache Software License',
                'Intended Audience :: Developers',
                'Programming Language :: Python :: 3.8',
                'Topic :: Software Development',
                'Topic :: System :: Hardware']
 
 setup(
-    name         = 'IMU_LSM9DS1',
+    name         = 'IMU_lsm9ds1',
     version      = '1.0.1',
     author       = 'Maciej Biskup',
     author_email = 'mbiskup123@gmail.com',
     description  = "Library for lsm9ds1",
     long_description=long_description,
     url          = 'https://github.com/electron001/imu/',
-    license      = 'Apache License 2.0',
+    license      = 'Apache Software License',
     classifiers  = classifiers,
     packages     = find_packages()
 )
